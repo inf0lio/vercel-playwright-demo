f416da42792f35262399090a5fc847a686ab5f96
Danylo Varfolomieiev <danylo.varfolomieiev@userway.org>
2023-12-27 13:52:25 +0200

    test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
+  console.log(process.env.PLAYWRIGHT_TEST_BASE_URL);
+
 
   // Expect a title "to contain" a substring.
   await expect(page.locator('.p-3.text-capitalize')).toHaveText(/you can drag and drop Items : /);
